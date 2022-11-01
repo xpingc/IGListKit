09e6e2be5371509dee0c5d2cb1a3c1d7ac4a8dc4
xpingc <xiaoping@outlook.com>
2022-11-01 10:45:06 -0700

    Add image property in PokemonEntry

--- my_exapmles/MarsLink_Materials/MarsLink_Materials/Marslink_MyTest/Marslink/Models/PokemonEntry.swift
+++ my_exapmles/MarsLink_Materials/MarsLink_Materials/Marslink_MyTest/Marslink/Models/PokemonEntry.swift
@@ -27,12 +27,17 @@
 /// THE SOFTWARE.
 
 import Foundation
+import UIKit
 
 class Pokemon: NSObject {
   let name: String
+  let image: UIImage
   
   init(name: String) {
     self.name = name
+    self.image = UIImage(named: name,
+                         in: Bundle(for: type(of:self)),
+                         compatibleWith: nil)!
   }
 }
 
